f4c52b5e41734e3592cb0e158fee07afa7823ff2
Anders Elo <anders@kaerus.com>
2013-08-12 19:52:06 +0200

    refactores, fixes & cleanup

--- README.md
+++ README.md
@@ -5,242 +5,105 @@
 [![Build Status](https://travis-ci.org/kaerus-component/promise.png)](https://travis-ci.org/kaerus-component/promise.js)
 Promise.js
 ==========
-A Promises/A+ library, originally developed for the <a href="https://github.com/kaerus/arango-client">ArangoDB client</a>. 
+Providing A+ compliant promises with some extras. Based on [microPromise (uP)](https://github.com/kaerus-component/uP)
 
-Main features
--------------
-* Conforming to the Promises/A+ specification.
-* Small footprint, no other dependencies or frameworks.
-* Simple design, fast, easy to understand, integrate & debug.
-* Runtime independent so that it works in browsers, nodejs and perhaps even in rhino. 
-* Supports CommonJS and AMD loaders, or exports a global Promise(). 
-* Flexible fulfillments that allows multiple fulfillment values catched with then(value) or spread(arguments).
-* Easy to create promised wrappers by using when(task) to bundle functions/processes into a promise. 
-* Additional control through defer(), attached(), timout() & abort() methods.
+- [Promise.async()](#promiseasync)
+- [Promise.async2()](#promiseasync2)
+- [Promise.join()](#promisejoinpromisesarray)
+- [Promise.wrap()](#promisewrapprotoobject)
+- [Promise.spread()](#promisespreadonfulfillfunctiononrejectfunction)
+- [Promise.timeout()](#promisetimeouttimenumbercallbackfunction)
+- [Promise.attach()](#promiseattachhandleobject)
+- [Promise.abort()](#promiseabortmessageobject)
 
-Install
-=======
-Include the promise component into your project using ```component install kaerus-component/promise```.
+## Promise.async()
 
-Test
-====
-The test framework requires nodejs.
-Install test dependencies through node package manager ```npm i -d``` and run ```npm test```.
-
-Usage
-=====
-Example a simple webpage using promises (note promise.js should be your component build file).
-```html
-<!doctype html>
-<html>
-	<head><title>Promise</title>
-		<script src="promise.js"></script>
-	</head>
-<body>
-	<h1 id="h1"></h1>
-	<h2 id="h2"></h2>
-	<h2 id="h3"></h2>
-	<script>
-		document.getElementById('h1').innerHTML = "Fulfill a promise";
-
-		Promise().fulfill("Hello").then(function(a){
-			var doc = document.getElementById('h2');
-			doc.innerHTML = a;
-			return "World!";
-		}).then(function(b){
-			var doc = document.getElementById('h3');
-			doc.innerHTML = b;
-		});
-	</script>
-</body>
-</html>
+  Makes a process/function asynchronous.
+  The process may also return a promise itself which to wait on.
+  If the process returns undefined the promise will remain pending.  
+  
+  Example: Make readFileSync async
+```js
+   fs = require('fs');
+   var asyncReadFile = p.async(fs.readFileSync,'./index.js');
+   asyncReadFile.then(function(data){
+       console.log(data.toString())
+   },function(error){
+       console.log("Read error:", error);
+   });
 ```
 
-Example of how to use promises for Ajax.
-```html
-<!doctype html>
-<html>
-    <head><title>Promise</title>
-        <script src="./promise.js"></script>
-    </head>
-<body>
-    <h1 id="h1"></h1>
-    <h2 id="h2"></h2>
-    <div id="content"></div>
-    <script>
-        document.getElementById('h1').innerHTML = "Ajaxed promise";
-        function getAjax(url) {
-        	var promise = new Promise(),
-        		xhr = new XMLHttpRequest, timer;
+## Promise.async2()
 
-            /* This will be called after getAjax(...) returns */
-            xhr.onreadystatechange = function() {
-            	if(xhr.readyState === 4 && xhr.status) {
-            		clearTimeout(timer);
-            		if(xhr.status > 399) promise.reject(xhr.statusText);
-                    else promise.fulfill(xhr.responseText);   	
-            	}
-            }
-
-            /* send an asynchronous XmlHttp GET request */
-            xhr.open("GET",url,true);
-            xhr.send(null);
-
-            /* set a timeout that fires after 5 secs */
-            timer = setTimeout(function() { 
-                xhr.abort();
-                promise.reject("operation timedout!"); 
-            },5000);
-
-            /* return this promise */
-        	return promise;
-        }
-
-        /* When the response arives it calls either the fulfilled block or the rejected block. */
-        getAjax("http://earthquake.usgs.gov/earthquakes/feed/geojson/significant/month").then(function(value){
-            /* validates JSON data */
-            var data = JSON.parse(value);
-        	document.getElementById('h2').innerHTML = "Fulfilled";
-            document.getElementById('content').innerHTML = JSON.stringify(data);
-        },function(reason){
-        	document.getElementById('h2').innerHTML = "Rejected";
-            document.getElementById('content').innerHTML = reason;
-        });
-    </script>
-</body>
-</html>
+  Adapted for processes expecting a callback(err,ret). 
+  
+  Example: make readFile async
+```js
+   fs = require('fs');
+   var asyncReadFile = p.async2(fs.readFile,'./index.js');
+   asyncReadFile.then(function(data){
+       console.log(data.toString())
+   },function(error){
+       console.log("Read error:", error);
+   });
 ```
 
-```javascript
-/* chaining promises, (undefined blocks are ignored) */
-promise = new Promise();
+## Promise.join(promises:Array)
 
-promise.then(function(value){
-	return value + 1;	
-}).then(function(value){
-	if(value < 5) throw "value is to small";
-	console.log("value:", value);
-}).then(undefined,function(error){
-	/* catch thrown error here */
-	console.log(error);
-});
-
-/* fulfill with a random value */
-promise.fulfill(Math.floor(Math.random()*10));
+  Joins promises and assembles return values into an array.
+  If any of the promises rejects the rejection handler is called with the error.  
+  
+  Example: join two promises
+```js
+   p = Promise();
+   a = Promise();
+   b = Promise();
+   p.join([a,b]).spread(function(x,y){
+       console.log('a=%s, b=%s',x,y);
+   },function(err){
+       console.log('error=',e);
+   });
+   b.fulfill('hello');
+   a.fulfill('world'); // => 'a=hello, b=world' 
+   p.resolved; // => ['hello','world']
 ```
 
+## Promise.wrap(proto:Object)
 
-```javascript
-/* Multiple fulfillment values can be placed inside an array */
-promise().fulfill([1,2,3]).then(function(value) {
-	console.log("a(%s) b(%s) c(%s)", value[0], value[1], value[2]);
-});
-
-/* Use spread() to receive the individual elements as arguments */
-promise().fulfill([1,2,3]).spread(function(a,b,c) {
-	console.log("a(%s) b(%s) c(%s)", a, b, c);
-});
-
-/* You can also fulfill with multiple fullfilment arguments. */
-promise().fulfill("abc",123,{abc:123}).spread(function(a,b,c) {
-	console.log("a(%s) b(%s) c(%s)", a, b, c);
-});
-
+  Wraps a `proto`
+  
+  Example: wrap an Array
+```js
+   p = Promise();
+   c = p.wrap(Array);
+   c(1,2,3); // => calls constructor and fulfills promise 
+   p.resolved; // => [1,2,3]
 ```
 
-Example how to create a promise around a process making it run asynchronously in your code.
-```javascript
-var promisedSync = Promise().when(function(){
-	var retval = syncProc();
-	/* if syncProc() throws the error will */
-	/* end up in the rejection handler.    */
-	if(retval) this.fulfill(retval);
-	else this.reject("Operation failed");
-});
-/* Process the result whenever its ready */
-promisedSync.then(...);
+## Promise.spread(onFulfill:Function, onReject:Function)
+
+  Spread has the same semantic as then() but splits multiple fulfillment values into separate arguments  
+  
+  Example: Fulfillment array elements as arguments
+```js
+   var p = uP();
+   p.fulfill([1,2,3]).spread(function(a,b,c){
+       console.log(a,b,c); // => 123
+   });
 ```
 
-You may also wrap a promise around multiple functions or promises.
-```javascript
-var tasks = [];
-tasks.push(someFunction);
-tasks.push(somePromise);
-Promise().when(tasks).then(function(values){
-	console.log("Tasks returned", values);
-}, function(error){
-	console.log("Tasks failed", error);
-});
-```
+## Promise.timeout(time:Number, callback:Function)
 
-Example with a more refined Ajax wrapper using attached(), abort() & timeout().
-```javascript
-// ajax wrapper returning a promise
-function Ajax(options,data) {
-    var res = new Promise(),
-        req = new XMLHttpRequest;
+  Timeout a pending promise and invoke callback function on timeout.
+  Without a callback it throws a RangeError('exceeded timeout').
 
-    options = options ? options : {};
-    data = data ? data : null;
+## Promise.attach(handle:Object)
 
-    if(typeof options !== 'object') options = {url:options};
-    if(!options.method) options.method = "get";
-    if(!options.headers) options.headers = {};
-    if(!options.timeout) options.timeout = 5000;
-    if(!options.headers.accept) options.headers.accept = "application/json";
+  Attaches a `handle`. In an Ajax scenario this could be the xhr object
 
-    res.attach(req);
+## Promise.abort(message:Object)
 
-    function handle(req,res){       
-        var msg = req.responseText,
-            hdr = parseHeaders(req.getAllResponseHeaders());
-
-        if(options.headers.accept.indexOf('json') >= 0) 
-            msg = JSON.parse(msg);
-
-        if(req.status < 400) res.fulfill(msg,hdr);
-        else res.reject(msg);  
-    }
-
-    function parseHeaders(h) {
-        var ret = {}, key, val, i;
-
-        h.split('\n').forEach(function(header) {
-            if((i=header.indexOf(':')) > 0) {
-                key = header.slice(0,i).replace(/^[\s]+|[\s]+$/g,'').toLowerCase();
-                val = header.slice(i+1,header.length).replace(/^[\s]+|[\s]+$/g,'');
-                if(key && key.length) ret[key] = val;
-            }   
-        });
-
-        return ret;
-    }
-
-    req.onreadystatechange = function() {
-        if(req.readyState === 4 && req.status) {
-            /* clear response timer */
-            res.timeout(null);
-            handle(req,res);   
-        }
-    }
-
-    /* send an asynchronous XmlHttp request */
-    req.open(options.method,options.url,true);
-    
-    /* set request headers */
-    Object.keys(options.headers).forEach(function(header) {
-        req.setRequestHeader(header,options.headers[header]);
-    });
-
-    /* send request */
-    req.send(data);
-
-    /* set a timeout */
-    res.timeout(options.timeout);
-
-    return res;
-}
-```
+  Aborts pending request by attemtping to call `attached.abort()` and then rejecting promise.
 
 
 License
--- index.js
+++ index.js
@@ -2,192 +2,235 @@
 var uP = require('uP');
 
 (function(){
-    function Promise(o){
-
-        var timer;
-
-        var Methods = {
-
-            /**
-             * Makes a process/function asynchronous.
-             * The process may also return a promise itself which to wait on.
-             * If the process returns undefined the promise will remain pending.  
-             * 
-             * Example: Make readFileSync async
-             *      fs = require('fs');
-             *      var asyncReadFile = p.async(fs.readFileSync,'./index.js');
-             *      asyncReadFile.then(function(data){
-             *          console.log(data.toString())
-             *      },function(error){
-             *          console.log("Read error:", error);
-             *      });
-             *         
-             * @return {Object} promise
-             * @api public
-             */
-            async: function(){
-                var self = this,
-                    args = Array.prototype.slice.call(arguments),
-                    proc = args.shift(),
-                    ret;
-
-                task(function(){
-                    try {
-                        ret = proc.apply(null,args);
-                        if(isPromise(ret)) ret.then(self.fulfill,self.reject);
-                        else if(ret !== undefined) self.fulfill(ret);
-                    } catch (e) {
-                        self.reject(e);
-                    }
-                });
-
-                return this;
-            },
-            /**
-             * Adapted for processes expecting a callback(err,ret). 
-             * 
-             * Example: make readFile async
-             *      fs = require('fs');
-             *      var asyncReadFile = p.async2(fs.readFile,'./index.js');
-             *      asyncReadFile.then(function(data){
-             *          console.log(data.toString())
-             *      },function(error){
-             *          console.log("Read error:", error);
-             *      });
-             *         
-             * @return {Object} promise
-             * @api public
-             */
-            async2: function(){
-                var self = this,
-                    args = Array.prototype.slice.call(arguments);
-
-                function callback(err,ret){ if(!e) self.fulfill(ret); else self.reject(ret); }
-
-                args[args.length] = callback;
-
-                return this.async.apply(this,args);
-            },
-
-            /**
-             * Joins promises and assembles return values into an array.
-             * If any of the promises rejects the rejection handler is called with the error.  
-             * 
-             * Example: join two promises
-             *      p = uP();
-             *      a = uP();
-             *      b = uP();
-             *      p.join([a,b]).spread(function(x,y){
-             *          console.log('a=%s, b=%s',x,y);
-             *      },function(err){
-             *          console.log('error=',e);
-             *      });
-             *      b.fulfill('hello');
-             *      a.fulfill('world'); // => 'a=hello, b=world' 
-             *      p.resolved(); // => ['hello','world']
-             *
-             * @param {Array} promises
-             * @return {Object} promise
-             * @api public
-             */
-            join: function(promises){
-                var val = [], 
-                    self = this, 
-                    chain = uP().fulfill();
-
-                if(!Array.isArray(promises)) promises = [promises];
-
-                function collect(i){
-                    promises[i].then(function(v){
-                        val[i] = v;
-                    });
-
-                    return function(){return promises[i]}    
-                }
-
-                for(var i = 0, l = promises.length; i < l; i++){
-                    chain = chain.then(collect(i));
-                }
-
-                chain.then(function(){self.fulfill(val)},function(e){self.reject(e)});
-
-                return this;
-            },
-
-
-            wrap: function(proto){
-                var self = this;
-
-                return function(){
-                    var args = Array.prototype.slice.call(arguments), ret;
-
-                    if(isPromise(proto)){
-                        proto.fulfill(args).then(self.fulfill,self.reject);
-                    } else if(typeof proto === 'function'){
-                        try{
-                            ret = proto(args);
-                            self.fulfill(ret);
-                        } catch(err) {
-                            self.reject(err);
-                        }
-                    }
-                        
-                    return self;
-                }              
-            },
-
-
-            /**
-             * Spread has the same semantic as then() but splits multiple fulfillment values & rejection reasons into separate arguments  
-             * 
-             * Example: Fulfillment array elements as arguments
-             *      var p = uP();
-             *      p.fulfill([1,2,3]).spread(function(a,b,c){
-             *          console.log(a,b,c); // => 123
-             *      });     
-             *      
-             * @param {Function} onFulfill callback with multiple arguments
-             * @param {Function} onReject errback with multiple arguments  
-             * @return {Object} promise for chaining
-             * @api public
-             */
-            spread: function(f,r){    
-                function s(h){
-                    return function(v){
-                        if(!Array.isArray(v)) v = [v];
-                        return h.apply(null,v); 
-                    }
-                }
-
-                return this.then(s(f),s(r));
-            },
-
-            /**
-             * Timeout a pending promise and invoke callback function on timeout.
-             * Without a callback it throws a RangeError('exceeded timeout').
-             * 
-             * @param {Number} time timeout value in ms or null to clear timeout
-             * @param {Function} callback optional timeout function callback
-             * @throws {RangeError} If exceeded timeout  
-             * @return {Object} promise
-             * @api public
-             */
-            timeout: function(t,f){
-                var self = this;
-
-                if(t === null || state) {
-                    clearTimeout(timer);
-                    timer = null;
-                } else if(!timer){
-                    f = f ? f : function(){ self.reject(RangeError("exceeded timeout")) }
-                    timer = G.setTimeout(f,t);
-                }       
-
-                return this;
-            }
-        };
-
-        return uP(Methods);
+    
+    function isPromise(f) {
+        return f && typeof f.then === 'function';
     }
+
+    function Promise(){
+        return uP(this);
+    }
+
+    /**
+     * Makes a process/function asynchronous.
+     * The process may also return a promise itself which to wait on.
+     * If the process returns undefined the promise will remain pending.  
+     * 
+     * Example: Make readFileSync async
+     *      fs = require('fs');
+     *      var asyncReadFile = p.async(fs.readFileSync,'./index.js');
+     *      asyncReadFile.then(function(data){
+     *          console.log(data.toString())
+     *      },function(error){
+     *          console.log("Read error:", error);
+     *      });
+     *         
+     * @return {Object} promise
+     * @api public
+     */
+    Promise.prototype.async = function(){
+        var self = this,
+            args = Array.prototype.slice.call(arguments),
+            proc = args.shift(),
+            ret;
+
+        this.defer(function(){
+            proc.apply(null,args);
+        });
+
+        return this;
+    };
+
+    /**
+     * Adapted for processes expecting a callback(err,ret). 
+     * 
+     * Example: make readFile async
+     *      fs = require('fs');
+     *      var asyncReadFile = p.async2(fs.readFile,'./index.js');
+     *      asyncReadFile.then(function(data){
+     *          console.log(data.toString())
+     *      },function(error){
+     *          console.log("Read error:", error);
+     *      });
+     *         
+     * @return {Object} promise
+     * @api public
+     */
+    Promise.prototype.async2 = function(){
+        var self = this,
+            args = Array.prototype.slice.call(arguments);
+
+        function callback(err,ret){ if(!e) self.fulfill(ret); else self.reject(ret); }
+
+        args[args.length] = callback;
+
+        return this.async.apply(this,args);
+    };
+
+    /**
+     * Joins promises and assembles return values into an array.
+     * If any of the promises rejects the rejection handler is called with the error.  
+     * 
+     * Example: join two promises
+     *      p = Promise();
+     *      a = Promise();
+     *      b = Promise();
+     *      p.join([a,b]).spread(function(x,y){
+     *          console.log('a=%s, b=%s',x,y);
+     *      },function(err){
+     *          console.log('error=',e);
+     *      });
+     *      b.fulfill('hello');
+     *      a.fulfill('world'); // => 'a=hello, b=world' 
+     *      p.resolved; // => ['hello','world']
+     *
+     * @param {Array} promises
+     * @return {Object} promise
+     * @api public
+     */
+    Promise.prototype.join = function(promises){
+        var val = [], 
+            self = this, 
+            chain = uP().fulfill();
+
+        if(!Array.isArray(promises)) promises = [promises];
+
+        function collect(i){
+            promises[i].then(function(v){
+                val[i] = v;
+            });
+
+            return function(){return promises[i]}    
+        }
+
+        for(var i = 0, l = promises.length; i < l; i++){
+            chain = chain.then(collect(i));
+        }
+
+        chain.then(function(){self.fulfill(val)},function(e){self.reject(e)});
+
+        return this;
+    };
+
+    /**
+     * Wraps a `proto`
+     * 
+     * Example: wrap an Array
+     *      p = Promise();
+     *      c = p.wrap(Array);
+     *      c(1,2,3); // => calls constructor and fulfills promise 
+     *      p.resolved; // => [1,2,3]
+     *
+     * @param {Object} proto
+     * @return {Object} promise
+     * @api public
+     */
+    Promise.prototype.wrap = function(proto){
+        var self = this;
+
+        return function(){
+            var args = Array.prototype.slice.call(arguments), ret;
+
+            if(isPromise(proto)){
+                proto.fulfill(args).then(self.fulfill,self.reject);
+            } else if(typeof proto === 'function'){
+                try{
+                    ret = proto.apply(null,args);
+                    self.fulfill(ret);
+                } catch(err) {
+                    self.reject(err);
+                }
+            }
+                
+            return self;
+        }              
+    };
+
+    /**
+     * Spread has the same semantic as then() but splits multiple fulfillment values into separate arguments  
+     * 
+     * Example: Fulfillment array elements as arguments
+     *      var p = uP();
+     *      p.fulfill([1,2,3]).spread(function(a,b,c){
+     *          console.log(a,b,c); // => 123
+     *      });     
+     *      
+     * @param {Function} onFulfill
+     * @param {Function} onReject  
+     * @return {Object} promise for chaining
+     * @api public
+     */
+    Promise.prototype.spread = function(f,r){    
+        function s(v){
+            if(!Array.isArray(v)) v = [v];
+            return f.apply(this,v); 
+        }
+
+        return this.then(s,r);
+    };
+
+    /**
+     * Timeout a pending promise and invoke callback function on timeout.
+     * Without a callback it throws a RangeError('exceeded timeout').
+     * 
+     * @param {Number} time timeout value in ms or null to clear timeout
+     * @param {Function} callback optional timeout function callback
+     * @throws {RangeError} If exceeded timeout  
+     * @return {Object} promise
+     * @api public
+     */
+    Promise.prototype.timeout = function(t,f){
+        var self = this;
+
+        if(t === null) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        } else if(!this.timer){
+            if(typeof f !== 'function'){
+                f = function(){ 
+                    if(self.status ==='pending') 
+                        self.abort(RangeError("exceeded timeout")); 
+                }
+            }    
+            this.timer = setTimeout(f,t);
+        }       
+
+        return this;
+    };
+
+    /**
+     * Attaches a `handle`. In an Ajax scenario this could be the xhr object
+     *
+     * @param {Object} handle
+     * @returns {Object} promise
+     * @api public
+     */
+    Promise.prototype.attach = function(handle){
+         
+        Object.defineProperty(this,'attached',{
+            enumerable:false,
+            value: handle
+        });
+
+        return this;
+    };
+
+    /**
+     * Aborts pending request by attemtping to call `attached.abort()` and then rejecting promise. 
+     *
+     * @param {Object} message
+     * @returns {Object} promise
+     * @api public
+     */
+    Promise.prototype.abort = function(message){
+        if(this.attached && typeof this.attached.abort === 'function')
+            this.attached.abort(message);
+
+        this.reject(message);
+
+        return this;
+    };
+
+
     module.exports = Promise;   
 })();
